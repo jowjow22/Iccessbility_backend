8c9b3a9c84a0d0fd901c37a39b5ea08c02a0252e
jowjow22 <jonathan224santos@gmail.com>
2020-11-19 16:56:33 -0300

    repairs

--- src/index.js
+++ src/index.js
@@ -2,30 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const io = require('socket.io');
-
-const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
-
-const server = http.createServer(http);
-
-
-io = io(server);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(routes);
 
-io.on('connection', socket =>{
-    console.log('new Connection');
-});
+app.use(routes);
 
 app.listen( process.env.PORT || 3333, ()=>{
     console.log('Back-end started...');

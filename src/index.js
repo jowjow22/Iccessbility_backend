d997f2bea565303ddc20c708af5f1bbc73ada4c6
jowjow22 <jonathan224santos@gmail.com>
2020-11-21 22:02:38 -0300

    added cors options

--- src/index.js
+++ src/index.js
@@ -6,21 +6,12 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use('*', cors());
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
 app.use(routes);
 
 app.listen( process.env.PORT || 3333, ()=>{

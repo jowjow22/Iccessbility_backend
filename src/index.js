5b1aa5aa5c5b8e7c7da34a63a664363f9a9a46bd
jowjow22 <jonathan224santos@gmail.com>
2020-11-22 13:58:27 -0300

    repairs

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(routes);
 
 app.listen( process.env.PORT || 3333, ()=>{
